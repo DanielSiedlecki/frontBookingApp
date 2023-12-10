0a68a9082ae6ef72e06f2caac60f5712371c47a4
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-11 00:49:33 +0100

    implement tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        image: "url('./assets/backgroundPhoto.jpg')",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
