8c2a42f7502b138c1d99be99277e1b330c17f1cd
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-11 01:07:31 +0100

    implement HomeSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
-import EventPicker from './components/EventPicker';
+import HomeSection from './components/homeSection';
 
 function App() {
   return (
     <div className="h-screen w-screen bg-image flex items-center justify-center">
-      <div className="content hidden justify-center items-center flex-col w-11/12 h-96 lg:w-5/12 lg:h-72 bg-white rounded-lg">
-        <h1 className='mb-10'>Barber Shop</h1>
-        <button className='w-40'>Umów wizytę</button>
-      </div>
-      <EventPicker />
+      <HomeSection/>
     </div>
   );
 }
