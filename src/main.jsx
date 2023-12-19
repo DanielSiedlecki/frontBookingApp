1daa8cae58cb4fdfb12517b92ce55a5d633cbd60
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-19 20:46:37 +0100

    deleted dotenv import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
-import dotenv from 'dotenv'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
