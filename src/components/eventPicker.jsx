b367a8d8c2bfc9d839cf756ff4f3cb6144b50262
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-19 20:46:53 +0100

    define eventPicker and style

--- src/components/eventPicker.jsx
+++ src/components/eventPicker.jsx
@@ -1,13 +1,26 @@
-import { Datepicker } from 'flowbite-react';
+import { Datepicker } from "flowbite-react";
 
+import EmployeesList from "./employeesList";
 function EventPicker() {
-    return (
-        <div className="content text-center">
+  return (
+    <div className="content text-center">
+      <div className="choose-date">
         <p>Wybierz datę wizyty</p>
-        <Datepicker language="pl" className='' labelTodayButton="Dzisiaj" labelClearButton="Wyczyść"
-        onSelectedDateChanged={(date) => {
-        console.log(date)
-    }}/></div>)
+        <Datepicker
+          language="pl"
+          className=""
+          labelTodayButton="Dzisiaj"
+          labelClearButton="Wyczyść"
+          onSelectedDateChanged={(date) => {
+            console.log(date);
+          }}
+        />
+          </div>
+          <div className="choose-employee">
+              <EmployeesList></EmployeesList>
+              </div>
+    </div>
+  );
 }
 
 export default EventPicker;
