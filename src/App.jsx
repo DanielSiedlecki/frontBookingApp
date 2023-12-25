9600f5b431f5957a721fe9fef767025ed40afae9
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-25 18:44:18 +0100

    implement react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
+import CancelEventPage from './components/cancelEvenetPage';
 import HomeSection from './components/homeSection';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="bg-image flex items-center justify-center">
-      <HomeSection/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeSection/>} index />
+        <Route path="/events/cancelEvent/:id" element={<CancelEventPage/>} /> 
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
