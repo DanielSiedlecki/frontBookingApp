a1989089f2d13164365e465af470ef9485b7098e
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-25 19:41:10 +0100

    remove width style

--- src/components/homeSection.jsx
+++ src/components/homeSection.jsx
@@ -2,7 +2,7 @@ import ServiceList from "./servicesList";
 import InformationSection from "./informationSection";
 function HomeSection() {
   return (
-    <div className="content flex items-center flex-col bg-white rounded-lg w-screen lg:w-10/12">
+    <div className="content flex items-center flex-col bg-white rounded-lg w-screen lg:w-full">
       <div className="content-container flex flex-col lg:flex-row items-center lg:items-start w-9/12 mt-10 gap-5 ">
         <div className="services-section basis-full lg:basis-4/5 w-full  md:w-96">
           <ServiceList></ServiceList>
