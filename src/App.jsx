f16b0bd7720a81aafd19a44a4f9b8141a7524871
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-29 15:06:41 +0100

    define auth routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css';
 import CancelEventPage from './components/cancelEventPage';
+import LoginSection from './components/loginSection';
 import HomeSection from './components/homeSection';
+import RegisterSection from './components/registerSection';
+import ForgotPassword from './components/forgotPasswordSection';
+import ChangePassword from './components/changePasswordSection';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,7 +12,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomeSection/>} index />
-        <Route path="/events/cancelEvent/:id" element={<CancelEventPage/>} /> 
+        <Route path="/events/cancelEvent/:id" element={<CancelEventPage />} /> 
+        <Route path="/register" element={<RegisterSection />} /> 
+        <Route path="/login" element={<LoginSection />} /> 
+        <Route path="/forgotpassword" element={<ForgotPassword />} /> 
+        <Route path="/changepassword" element={<ChangePassword />} /> 
       </Routes>
     </Router>
   );
