c9d498e0131425cbe441342a60e2a0e6df20c22e
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-12-29 15:49:55 +0100

    implement isemailunique logic

--- src/components/registerSection.jsx
+++ src/components/registerSection.jsx
@@ -7,19 +7,25 @@ async function register(data) {
       name: data.name,
       surname: data.surname,
       email: data.email,
-      password:data.password
+      password: data.password,
     };
-    const reg = new registerUser()
+
+    const reg = new registerUser();
     await reg.post(eventData);
 
-    return true;
+    return { success: true };
   } catch (err) {
+    if (err.response && err.response.status === 409) {
+      return { success: false, error: "Email is already in use" };
+    }
+
     console.error(err);
-    return false;
+    return { success: false, error: "Registration failed" };
   }
 }
 
 function RegisterSection() {
+  const [isEmailUnique, setIsEmailUnique] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [formData, setFormData] = useState({
     name: "",
@@ -75,40 +81,123 @@ function RegisterSection() {
     } else if (formData.password.length < 6) {
       alert("Hasło jest za krótkie");
     } else {
-      const registrationSuccessful = await register(formData);
-      console.log(formData)
-      if (registrationSuccessful) {
+      const result = await register(formData);
+
+      if (result.success) {
         console.log("Pomyślnie zarejestrowany", formData);
         setIsModalOpen(true);
-        
       } else {
-        console.log("Błąd podczas rejestracji");
+        console.log("Błąd podczas rejestracji:", result.error);
+
+        if (result.error === "Email is already in use") {
+          setIsEmailUnique(false);
+        }
       }
     }
   };
 
-
   return (
     <div>
-      {!isModalOpen &&
-      <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
-        <div>
-          <a href="/">
-            <h3 className="text-4xl font-bold text-purple-600">Rejestracja</h3>
-          </a>
-        </div>
-        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
-          <form onSubmit={handleSubmit}>
-            <div className="fullname flex gap-2">
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Imię
+      {!isModalOpen && (
+        <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
+          <div>
+            <a href="/">
+              <h3 className="text-4xl font-bold text-purple-600">
+                Rejestracja
+              </h3>
+            </a>
+          </div>
+          <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
+            <form onSubmit={handleSubmit}>
+              <div className="fullname flex gap-2">
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Imię
+                  </label>
+                  <div className="flex flex-col items-start">
+                    <input
+                      type="text"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleInputChange}
+                      className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
+                        isEmpty.name ? "border-red-500" : ""
+                      }`}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Nazwisko
+                  </label>
+                  <div className="flex flex-col items-start">
+                    <input
+                      type="text"
+                      name="surname"
+                      value={formData.surname}
+                      onChange={handleInputChange}
+                      className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
+                        isEmpty.surname ? "border-red-500" : ""
+                      }`}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
                 </label>
                 <div className="flex flex-col items-start">
                   <input
                     type="text"
-                    name="name"
-                    value={formData.name}
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
+                      isEmpty.email
+                        ? "border-red-500"
+                        : isEmailUnique
+                        ? ""
+                        : "border-red-500"
+                    }`}
+                  />
+                  {!isEmailUnique && (
+                    <span className="text-red-500">
+                      Email is already in use
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div className="mt-4">
+                <label>Hasło</label>
+                <div className="flex flex-col items-start">
+                  <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
+                      isEmpty.email ? "border-red-500" : ""
+                    }`}
+                  />
+                </div>
+              </div>
+              <div className="mt-4">
+                <label
+                  htmlFor="password_confirmation"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Powtórz hasło
+                </label>
+                <div className="flex flex-col items-start">
+                  <input
+                    type="password"
+                    name="password_confirmation"
+                    value={formData.password_confirmation}
                     onChange={handleInputChange}
                     className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
                       isEmpty.name ? "border-red-500" : ""
@@ -116,102 +205,30 @@ function RegisterSection() {
                   />
                 </div>
               </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Nazwisko
-                </label>
-                <div className="flex flex-col items-start">
-                  <input
-                    type="text"
-                    name="surname"
-                    value={formData.surname}
-                    onChange={handleInputChange}
-                    className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
-                      isEmpty.surname ? "border-red-500" : ""
-                    }`}
-                  />
-                </div>
+              <div className="flex items-center justify-end mt-4">
+                <a
+                  className="text-sm text-gray-600 underline hover:text-gray-900"
+                  href="/login"
+                >
+                  Jesteś zarejestrowany?
+                </a>
+                <button
+                  onClick={handleSubmit}
+                  type="submit"
+                  className="inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900"
+                >
+                  Zarejestruj
+                </button>
               </div>
-            </div>
-
-            <div className="mt-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="text"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
-                    isEmpty.email ? "border-red-500" : ""
-                  }`}
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label>Hasło</label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleInputChange}
-                  className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
-                    isEmpty.email ? "border-red-500" : ""
-                  }`}
-                />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label
-                htmlFor="password_confirmation"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Powtórz hasło
-              </label>
-              <div className="flex flex-col items-start">
-                <input
-                  type="password"
-                  name="password_confirmation"
-                  value={formData.password_confirmation}
-                  onChange={handleInputChange}
-                  className={`block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${
-                    isEmpty.name ? "border-red-500" : ""
-                  }`}
-                />
-              </div>
-            </div>
-            <div className="flex items-center justify-end mt-4">
-              <a
-                className="text-sm text-gray-600 underline hover:text-gray-900"
-                href="/login"
-              >
-                Jesteś zarejestrowany?
-              </a>
-              <button
-                onClick={handleSubmit}
-                type="submit"
-                className="inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900"
-              >
-                Zarejestruj
-              </button>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-        </div>
-      }
+      )}
       {isModalOpen && (
         <div className="w-screen h-96 flex items-center justify-center">
           <div className="modal-overlay justify-center items-center flex flex-col gap-2 ">
             <i className="bi bi-check text-green-400 text-6xl"></i>
-            <p className="text-center">
-              Konto zostało utworzone
-            </p>
+            <p className="text-center">Konto zostało utworzone</p>
             <a href="/login">
               <button className="inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900">
                 Zaloguj się
