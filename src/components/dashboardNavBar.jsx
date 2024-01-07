b23cea2df7b278fabb9d49ed9772de39df4f8207
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-08 00:19:54 +0100

    implemented date fucntion

--- src/components/dashboardNavBar.jsx
+++ src/components/dashboardNavBar.jsx
@@ -1,24 +1,36 @@
 import { currentTime } from "../utils/data_utils/clock_currentTime";
+import { actualDate } from "../utils/data_utils/actualDate";
 import { useEffect, useState } from "react";
 
 function DashboardNavBar() {
   const [actualTimeFullFormat, setActualTimeFullFormat] = useState(currentTime);
   const actualTime = actualTimeFullFormat.slice(0, -3);
-    
+  const nowDate = actualDate();
+  const nowDateSplitFormat = actualDate().slice(0, -5);
   useEffect(() => {
     setInterval(() => {
       setActualTimeFullFormat(currentTime());
     }, 1000);
-
-    
   }, []);
 
   return (
     <div className="bg-black h-screen w-20 lg:w-52">
       <div className="navbar-overlay">
         <ul className="flex flex-col gap-5 lg:gap-3 pt-40 p-3 items-center lg:items-start">
-                  <p className="text-white text-2xl self-center mb-10 hidden lg:block">{actualTimeFullFormat}</p>
-                  <p className="text-white text-xl self-center mb-10 block lg:hidden">{actualTime}</p>
+          <span>
+            <p className="text-white text-2xl self-center hidden lg:block">
+              {actualTimeFullFormat}
+            </p>
+            <p className="text-white text-xl self-center block lg:hidden">
+              {actualTime}
+            </p>
+            <p className="text-white text-2xl self-center hidden lg:block">
+              {nowDate}
+            </p>
+            <p className="text-white text-xl self-center block lg:hidden">
+              {nowDateSplitFormat}
+            </p>
+          </span>
           <li>
             <span className="flex gap-2 text-white text-lg">
               <i className="bi bi-house-fill text-2xl lg:text-lg"></i>
@@ -37,8 +49,7 @@ function DashboardNavBar() {
               <p className="hidden lg:block">Wyloguj</p>
             </span>
           </li>
-              </ul>
-              
+        </ul>
       </div>
     </div>
   );
