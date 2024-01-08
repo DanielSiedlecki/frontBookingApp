7b893266954899673ccb50d0906f252f6547df80
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-08 22:24:55 +0100

    implemented events route

--- src/components/dashboardSection.jsx
+++ src/components/dashboardSection.jsx
@@ -1,11 +1,16 @@
 import DashboardNavBar from "./dashboardNavBar";
+import DashboardEvents from "./dashboardEvents";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function DashboardSection() {
   return (
     <div className="w-screen h-screen">
-      <DashboardNavBar></DashboardNavBar>
+      <DashboardNavBar />
+      <Routes>
+
+        <Route path="/events/*" element={<DashboardEvents />} />
+      </Routes>
     </div>
   );
 }
-
 export default DashboardSection;
