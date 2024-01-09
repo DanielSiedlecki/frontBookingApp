8acac5445b6e79fca60031ca939189f88a97ce22
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-09 23:27:26 +0100

    defined route settings

--- src/components/dashboardSection.jsx
+++ src/components/dashboardSection.jsx
@@ -1,14 +1,15 @@
 import DashboardNavBar from "./dashboardNavBar";
 import DashboardEvents from "./dashboardEvents";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DashboardSettings from "./dashboardSettings";
 
 function DashboardSection() {
   return (
     <div className="w-screen h-screen">
       <DashboardNavBar />
       <Routes>
-
         <Route path="/events/*" element={<DashboardEvents />} />
+        <Route path="/settings/*" element={<DashboardSettings />} />
       </Routes>
     </div>
   );
