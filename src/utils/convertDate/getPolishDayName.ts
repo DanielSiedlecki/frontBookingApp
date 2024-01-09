91d467a93cfcb3c297c485ce4ca957f4f340d45c
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2024-01-09 23:23:40 +0100

    created and added translateDayToPolishName func

--- src/utils/convertDate/getPolishDayName.ts
+++ src/utils/convertDate/getPolishDayName.ts
@@ -12,3 +12,34 @@ export const getPolishDayName = (date) => {
 
     return daysOfWeek[dayOfWeekIndex];
 };
+
+export const translateDayToPolishName = (dayName) => {
+    const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    const polishDays = [
+        "Niedziela",
+        "Poniedziałek",
+        "Wtorek",
+        "Środa",
+        "Czwartek",
+        "Piątek",
+        "Sobota",
+    ];
+
+    const index = daysOfWeek.indexOf(dayName);
+
+    if (index !== -1) {
+        return polishDays[index];
+    } else {
+        // Jeśli nie znaleziono dopasowania, możesz zwrócić oryginalną nazwę
+        return dayName;
+    }
+};
